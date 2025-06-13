dfd0b61f1140adf6c0e3161d7f8d98e7708ab92f
Dan Lapid <dlapid@cloudflare.com>
2025-06-13 18:14:03 +0100

    Use Sqlite DOs

--- src/entry.py
+++ src/entry.py
@@ -1,5 +1,17 @@
-from workers import Response, handler
+from workers import Response, handler, DurableObject
+
+class DurableObjectExample(DurableObject):
+    def __init__(self, state, env):
+        super().__init__(state, env)
+
+    async def greet(self):
+        result = self.ctx.storage.sql.exec("SELECT 'Hello, World!' as greeting").one()
+        return result.greeting
+
 
 @handler
 async def on_fetch(request, env):
-    return Response("Hello World!")
+    id = env.ns.idFromName("A")
+    obj = env.ns.get(id)
+
+    return Response(await obj.greet())
